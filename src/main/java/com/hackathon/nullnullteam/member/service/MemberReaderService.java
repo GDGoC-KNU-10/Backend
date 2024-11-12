3c8f436a416d49655bfdd9ba3b28ab771134c1ed
geonny <kky8586696@naver.com>
2024-11-12 17:02:33 +0900

    `Feat: findMember 추가 (공통으로 쓰는 메서드)

---
+++ src/main/java/com/hackathon/nullnullteam/member/service/MemberReaderService.java
@@ -0,0 +1,24 @@
+package com.hackathon.nullnullteam.member.service;
+
+import com.hackathon.nullnullteam.global.constants.MessageConstants;
+import com.hackathon.nullnullteam.global.exception.EntityNotFoundException;
+import com.hackathon.nullnullteam.member.Member;
+import com.hackathon.nullnullteam.member.infrastructure.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberReaderService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public Member getMemberById(Long memberId){
+        return memberRepository.findById(memberId)
+                .orElseThrow(
+                        () -> new EntityNotFoundException(MessageConstants.USER_NOT_FOUND_MESSAGE)
+                );
+    }
+}
